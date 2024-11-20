1d02c280c6f93854b87e09bf167910e1c249f132
Pifer <pifernl@appstate.edu>
2024-11-20 16:44:17 -0500

    just has other planes start in different positions

--- src/trainer.py
+++ src/trainer.py
@@ -3,10 +3,10 @@ from plane import *
 
 def main():
     planes = [Plaine(cube_v, edges, surfaces, colors, np.array([1, 0, 0]), np.array([0,.2,0]), 0, 1, 0.02, 0.1, 10, 10)]
-    planes.append(Plaine(cube_v, edges, surfaces, colors, np.array([1, 0, 0]), np.array([0,.2,0]), 0, 1, 0.02, 0.1, 10, 10))
-    planes.append(Plaine(cube_v, edges, surfaces, colors, np.array([1, 0, 0]), np.array([0,.2,0]), 0, 1, 0.02, 0.1, 10, 10))
-    planes.append(Plaine(cube_v, edges, surfaces, colors, np.array([1, 0, 0]), np.array([0,.2,0]), 0, 1, 0.02, 0.1, 10, 10))
-    planes.append(Plaine(cube_v, edges, surfaces, colors, np.array([1, 0, 0]), np.array([0,.2,0]), 0, 1, 0.02, 0.1, 10, 10))
+    planes.append(Plaine(cube_v, edges, surfaces, colors, np.array([1, 0, 0]), np.array([0,.2,0]), 0, 1, 0.02, 0.1, 5, 5))
+    planes.append(Plaine(cube_v, edges, surfaces, colors, np.array([1, 0, 0]), np.array([0,.2,0]), 0, 1, 0.02, 0.1, 0, 0))
+    planes.append(Plaine(cube_v, edges, surfaces, colors, np.array([1, 0, 0]), np.array([0,.2,0]), 0, 1, 0.02, 0.1, -5, -5))
+    planes.append(Plaine(cube_v, edges, surfaces, colors, np.array([1, 0, 0]), np.array([0,.2,0]), 0, 1, 0.02, 0.1, -10, -10))
 
     pygame.init()
     pygame.key.set_repeat(100)
